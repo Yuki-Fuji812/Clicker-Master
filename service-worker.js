4de97f447d07edba3722786d8e068c12ad83b365
Yuki-Fuji812 <yuki_f_k_os_player@yahoo.co.jp>
2026-09-20 13:08:19 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,22 @@
-const CACHE_NAME = 'clicker-master-offline-v1';
+const CACHE_NAME = 'clicker-master-standard-v2';
 const APP_SHELL = ['./', './index.html', './manifest.json', './service-worker.js'];
+
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting())
+  );
 });
+
 self.addEventListener('activate', event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
 });
+
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
   event.respondWith(
